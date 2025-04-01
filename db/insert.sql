1bfee1e5c0ca8c9a0f7c349b1c95d4674a5f8fde
Alex Ortega <mortegalex27@outlook.es>
2025-04-01 17:41:13 -0600

    feat(api rest)! primer etapa de modificacion por requerimiento, y nueva estructura

--- db/insert.sql
+++ db/insert.sql
@@ -1,4 +1,4 @@
-INSERT INTO menu (menu, icon, path, es_mantenimiento) VALUES
+INSERT INTO menu (name, icon, path, is_main_menu) VALUES
 ('Cotizador', 'calculate', 'quoter', 0),
 ('Vehiculos', 'car', 'vehicles', 0),
 ('Clientes', 'users', 'customers', 1),
@@ -9,18 +9,21 @@ INSERT INTO menu (menu, icon, path, es_mantenimiento) VALUES
 ('Perfiles', 'profile', 'profiles', 1),
 ('Reportes', 'report', 'reports', 0),
 ('Subasta', 'store', 'aution', 1),
-('Tipos de clientes', 'users', 'types-of-customers', 1),
+('Tipos de clientes', 'users', 'types-of-customers', 1);
 
-INSERT INTO permiso (permiso) VALUES 
+INSERT INTO customer_type (name) VALUES
+('Particular'),
+('Inportador');
+
+INSERT INTO profile (name) VALUES 
+('Administrador');
+
+INSERT INTO permission (name) VALUES 
 ('Agregar'),
 ('Editar'),
 ('Ver'); 
 
-INSERT INTO perfil (perfil) VALUES 
-('Administrador');
-
-
-INSERT INTO perfil_memu_permiso (id, id_perfil, id_menu, id_permiso) VALUES
+INSERT INTO menu_permission_profile (id, profile_id, menu_id, permission_id) VALUES
 (uuid(), 1, 10,	3),
 (uuid(), 1,	8,	1),
 (uuid(), 1,	2,	3),
@@ -55,8 +58,114 @@ INSERT INTO perfil_memu_permiso (id, id_perfil, id_menu, id_permiso) VALUES
 (uuid(), 1,	11,	2),
 (uuid(), 1,	11,	3);
 
-INSERT INTO usuario (nombre, usuario, contrasenia, telefono, correo, id_perfil) VALUES 
-('Admin', 'admin', '$2a$08$pfPpnWYXvYzuXBDkqgzmMev13jo7QDnKNzuJzUwzV06.7VzDZwHJS', '--sin telefono--',	'--sin correo--', 1)
+INSERT INTO user (name, username, password, phone_number, email, profile_id) VALUES
+('Admin', 'admin', '$2a$08$pfPpnWYXvYzuXBDkqgzmMev13jo7QDnKNzuJzUwzV06.7VzDZwHJS', '--sin telefono--',	'--sin correo--', 1);
+
+INSERT INTO vehicle_type (name) VALUES
+('sedan'),
+('suv'),
+('grande');
+
+INSERT INTO transport_type (name) VALUES
+('terrestre'),
+('maritimo');
+
+INSERT INTO state (name) VALUES
+('Alabama'),
+('Alaska'),
+('Arizona'),
+('Arkansas'),
+('California'),
+('Colorado,'),
+('Connecticut'),
+('Delaware'),
+('Florida'),
+('Georgia'),
+('Hawaii'),
+('Idaho'),
+('Illinois'),
+('Indiana'),
+('Iowa'),
+('Kansas'),
+('Kentucky'),
+('Louisiana'),
+('Maine'),
+('Maryland'),
+('Massachusetts'),
+('Michigan'),
+('Minnesota'),
+('Mississippi'),
+('Missouri'),
+('Montana'),
+('Nebraska'),
+('Nevada'),
+('New Hampshire'),
+('New Jersey'),
+('New Mexico'),
+('New York'),
+('North Carolina'),
+('North Dakota'),
+('Ohio'),
+('Oklahoma'),
+('Oregon'),
+('Pennsylvania'),
+('Rhode Island'),
+('South Carolina'),
+('South Dakota'),
+('Tennessee'),
+('Texas'),
+('Utah'),
+('Vermont'),
+('Virginia'),
+('Washington'),
+('West Virginia'),
+('Wisconsin'),
+('Wyoming');
+
+INSERT INTO department (name) VALUES
+('Guatemala'),
+('Quetzaltenango'),
+('Sacatepéquez'),
+('Escuintla'),
+('Huehuetenango');
+
+INSERT INTO municipality (name, department_id) VALUES
+-- 📌 Guatemala (ID = 1)
+('Guatemala', 1),
+('Mixco', 1),
+('Villa Nueva', 1),
+('San Miguel Petapa', 1),
+('Santa Catarina Pinula', 1),
+-- 📌 Quetzaltenango (ID = 2)
+('Quetzaltenango', 2),
+('Coatepeque', 2),
+('San Juan Ostuncalco', 2),
+('La Esperanza', 2),
+('Olintepeque', 2),
+-- 📌 Sacatepéquez (ID = 3)
+('Antigua Guatemala', 3),
+('Ciudad Vieja', 3),
+('Jocotenango', 3),
+('Sumpango', 3),
+('San Lucas Sacatepéquez', 3),
+-- 📌 Escuintla (ID = 4)
+('Escuintla', 4),
+('Puerto San José', 4),
+('Santa Lucía Cotzumalguapa', 4),
+('Palín', 4),
+('Masagua', 4),
+-- 📌 Huehuetenango (ID = 5)
+('Huehuetenango', 5),
+('Malacatancito', 5),
+('Chiantla', 5),
+('Todos Santos Cuchumatán', 5),
+('San Pedro Soloma', 5);
+
+INSERT INTO sede (name, municipality_id, state_id) VALUES
+-- 📌 Estado unidos (ID = 1)
+('Delawer', null, 8),
+-- 📌 Guatemala (ID = 1)
+('Guatemala Zona 15', 1, null);
 
 INSERT INTO estado_importacion ('estado_importacion','index','color') VALUES
 ('Cotizacion Aprobada',0,'#ffbb96'),
